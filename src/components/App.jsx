6593909f27082ed053c4890a0b0a7cafa0896705
ojaswi-454 <aggojaswi@gmail.com>
2021-06-22 21:45:55 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import { div } from "prelude-ls";
+
+function App(){
+    return (
+        <div>
+            <Header />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
